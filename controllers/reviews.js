050c5b89ef6efc48b1cdb9307e019b359798b455
a <email@example>
2024-09-21 01:07:55 +0300

    first commit

---
+++ controllers/reviews.js
@@ -0,0 +1,22 @@
+const Campground = require('../models/campground');
+const Review = require('../models/review');
+
+module.exports.createReview = async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    const review = await Review.create({ author: req.user, ...req.body.review });
+
+    campground.reviews.push(review);
+    await campground.save();
+    req.flash('success', 'Created new review!');
+    res.redirect(`/campgrounds/${campground._id}`);
+}
+
+
+module.exports.deleteReview = async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    req.flash('success', 'Successfully deleted review');
+    res.redirect(`/campgrounds/${id}`);
+}
